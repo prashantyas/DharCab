903f78ba3cc11cf44cd4c273d6f0b2fcdf2be898
Prashant Vyas <prashant181vyas@gmail.com>
2025-01-10 16:17:02 +0530

    new theme

---
+++ wp-content/themes/raft/assets/js/build/design-pack-notice.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '736b6b98eef5c3b99b2a');
---
+++ wp-content/themes/raft/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/raft',
+        'pretty_version' => 'v1.1.7',
+        'version' => '1.1.7.0',
+        'reference' => '832de91743a1e23c5e9681f705f1ec7458824038',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/raft' => array(
+            'pretty_version' => 'v1.1.7',
+            'version' => '1.1.7.0',
+            'reference' => '832de91743a1e23c5e9681f705f1ec7458824038',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.37',
+            'version' => '3.3.37.0',
+            'reference' => '73e9010fb4e30ad8835f00a0df802733f98c4094',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
